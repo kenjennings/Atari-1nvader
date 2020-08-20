fa42c9f13cffd34b67a0cd1e793840211e6ea4a6
Ken Jennings <kj.sumter.42@gmail.com>
2020-08-20 17:48:16 -0400

    Add files via upload

--- ata_1nv_gfx.asm
+++ ata_1nv_gfx.asm
@@ -123,8 +123,8 @@
 ; 00 | 000000 P1      HI 000000     P2 000000 | (020 - 027) (2) P1 score, High score, P2 score
 ; 01 |                                        | (028 - 035) Blank 8
 ; 02 |                                        | (036 - 043) Blank 8
-; 03 |                   3                    | (044 - 051) (6) 3, 2, 1, GO line animation
-; 04 |                   3                    | (052 - 059) (6) 3, 2, 1, GO line animation
+; 03 |                   3                    | (044 - 051) Blank 8   3, 2, 1, GO line animation
+; 04 |                   3                    | (052 - 059) Blank 8   3, 2, 1, GO line animation
 ; 05 |                                        | (060 - 070) Blank 8 + Blank 3  (DLI vscroll hack) 
 ; 06 |           1NNNVVVAADDDEEERRR           | (071 - 073) (074 - 076) Mode F * 3 Animated Gfx 
 ; 07 |           1NNNVVVAADDDEEERRR           | (077 - 079) (080 - 082) Mode F * 3 Animated Gfx
@@ -148,7 +148,10 @@
 ;    ------------------------------------------
 
 DISPLAY_LIST_TITLE
-	.byte DL_BLANK_8, DL_BLANK_8, DL_BLANK_4|DL_DLI 
+	.byte DL_BLANK_8, DL_BLANK_8, DL_BLANK_4|DL_DLI   ; (000 - 019) Blank scan lines. 8+8+4 
+	mDL_LMS DL_TEXT_2|DL_DLI,GFX_SCORE_LINE           ; (020 - 027) (2) P1 score, High score, P2 score
+	.byte DL_BLANK_8
+
 
 BOTTOM_OF_DISPLAY                                 ; Prior to this DLI SPC1 set colors and HSCROL
 	mDL_LMS DL_TEXT_2,ANYBUTTON_MEM               ; (190-197) (+0 to +7)   Prompt to start game.
@@ -169,29 +172,29 @@ SCROLL_CREDIT_LMS = [* + 1]
 
 ;    ------------------------------------------ (000 - 019) Blank scan lines. 8+8+4
 ; 00 | 000000 P1      HI 000000     P2 000000 | (020 - 027) (2) P1 score, High score, P2 score
-; 01 |                                        | (028 - 035) (2) Stars
-; 02 |                                        | (036 - 043) (2) Stars
-; 03 |                                        | (044 - 051) (2) Stars
-; 04 |                                        | (052 - 059) (2) Stars
-; 05 |                                        | (060 - 067) (2) Stars
-; 06 |                                        | (068 - 075) (2) Stars
-; 07 |                                        | (076 - 083) (2) Stars
-; 08 |                                        | (084 - 091) (2) Stars
-; 09 |                                        | (092 - 099) (2) Stars
-; 10 |                                        | (100 - 107) (2) Stars
-; 11 |                                        | (108 - 115) (2) Stars
-; 12 |                                        | (116 - 123) (2) Stars
-; 13 |                                        | (124 - 131) (2) Stars
-; 14 |                                        | (132 - 139) (2) Stars
-; 15 |                                        | (140 - 147) (2) Stars
-; 16 |                                        | (148 - 155) (2) Stars
-; 17 |                                        | (156 - 163) (2) Stars
-; 18 |                                        | (164 - 171) (2) Stars
-; 19 |             Mountains                  | (172 - 179) (4) Mountains
-; 20 |             Mountains                  | (180 - 187) (4) Mountains
-; 21 |             Mountains                  | (188 - 195) (4) Mountains
-; 22 |             Mountains                  | (196 - 203) (4) Mountains
-; 23 |B   Solid ground and bumpers           B| (204 - 211) (4) Ground and bumpers, guns operational
+; 01 |                                        | (028 - 035) (6) Stars
+; 02 |                                        | (036 - 043) (6) Stars
+; 03 |                                        | (044 - 051) (6) Stars
+; 04 |                                        | (052 - 059) (6) Stars
+; 05 |                                        | (060 - 067) (6) Stars
+; 06 |                                        | (068 - 075) (6) Stars
+; 07 |                                        | (076 - 083) (6) Stars
+; 08 |                                        | (084 - 091) (6) Stars
+; 09 |                                        | (092 - 099) (6) Stars
+; 10 |                                        | (100 - 107) (6) Stars
+; 11 |                                        | (108 - 115) (6) Stars
+; 12 |                                        | (116 - 123) (6) Stars
+; 13 |                                        | (124 - 131) (6) Stars
+; 14 |                                        | (132 - 139) (6) Stars
+; 15 |                                        | (140 - 147) (6) Stars
+; 16 |                                        | (148 - 155) (6) Stars
+; 17 |                                        | (156 - 163) (6) Stars
+; 18 |                                        | (164 - 171) (6) Stars
+; 19 |             Mountains                  | (172 - 179) (6) Mountains
+; 20 |             Mountains                  | (180 - 187) (6) Mountains
+; 21 |             Mountains                  | (188 - 195) (6) Mountains
+; 22 |             Mountains                  | (196 - 203) (6) Mountains
+; 23 |B   Solid ground and bumpers           B| (204 - 211) (6) Ground and bumpers, guns operational
 ; 24 |]]]]]]]]]]]]]]]00 0000 00[[[[[[[[[[[[[[[| (212 - 219) (2) Ground, stats - line, score value, hits left
 ;    ------------------------------------------
 
@@ -229,7 +232,14 @@ SCROLL_CREDIT_LMS = [* + 1]
 ;    ------------------------------------------
 
 
-
+GFX_SCORE_LINE ; | 000000 P1      HI 000000     P2 000000 | 
+	.sb " "
+GFX_SCORE_P1 
+	.sb "000000            "
+GFX_SCORE_HI 
+	.sb "000000        "
+GFX_SCORE_P2
+	.sb "000000 "
 
 
 ; Title graphics are ANTIC F+GTIA $4 (aka BASIC mode 9, 16-grey scale) 
@@ -264,7 +274,7 @@ SCROLL_CREDIT_LMS = [* + 1]
 	; ...............X.X...X.XX..X.XXX.X...X.X.....X...X..............
 	; ...............X.X...X.X...X...X.X.XXX.XXXXX.X...X..............
 
-TITLE_FRAME1 ; 32 * 6 = 192
+GFX_TITLE_FRAME1 ; 32 * 6 = 192
 	.by $00 $00 $00 $00 $00 $00 $00 $03 $0C $00 $03 $06 $00 $0C $00 $03 $06 $9C $00 $03 $69 $C3 $0C $36 $90 $00 $00 $00 $00 $00 $00 $00
 	.by $00 $00 $00 $00 $00 $00 $00 $60 $09 $30 $0C $09 $00 $09 $00 $36 $0C $00 $30 $06 $00 $00 $09 $00 $0C $00 $00 $00 $00 $00 $00 $00
 	.by $00 $00 $00 $00 $00 $00 $00 $09 $06 $06 $09 $0C $00 $60 $0C $09 $09 $00 $06 $09 $0C $30 $06 $00 $03 $00 $00 $00 $00 $00 $00 $00
@@ -272,7 +282,7 @@ TITLE_FRAME1 ; 32 * 6 = 192
 	.by $00 $00 $00 $00 $00 $00 $00 $03 $0C $00 $03 $06 $C0 $06 $0C $93 $03 $00 $0C $03 $00 $00 $0C $00 $0C $00 $00 $00 $00 $00 $00 $00
 	.by $00 $00 $00 $00 $00 $00 $00 $06 $09 $00 $0C $09 $00 $03 $00 $06 $0C $09 $63 $06 $9C $36 $09 $00 $03 $00 $00 $00 $00 $00 $00 $00
 
-TITLE_FRAME2
+GFX_TITLE_FRAME2
 	.by $00 $00 $00 $00 $00 $00 $00 $0C $09 $00 $0C $03 $00 $09 $00 $0C $03 $69 $00 $0C $36 $9C $09 $C3 $60 $00 $00 $00 $00 $00 $00 $00
 	.by $00 $00 $00 $00 $00 $00 $00 $30 $06 $C0 $09 $06 $00 $06 $00 $C3 $09 $00 $C0 $03 $00 $00 $06 $00 $09 $00 $00 $00 $00 $00 $00 $00
 	.by $00 $00 $00 $00 $00 $00 $00 $06 $03 $03 $06 $09 $00 $30 $09 $06 $06 $00 $03 $06 $09 $C0 $03 $00 $0C $00 $00 $00 $00 $00 $00 $00
@@ -280,7 +290,7 @@ TITLE_FRAME2
 	.by $00 $00 $00 $00 $00 $00 $00 $0C $09 $00 $0C $03 $90 $03 $09 $6C $0C $00 $09 $0C $00 $00 $09 $00 $09 $00 $00 $00 $00 $00 $00 $00
 	.by $00 $00 $00 $00 $00 $00 $00 $03 $06 $00 $09 $06 $00 $0C $00 $03 $09 $06 $3C $03 $69 $C3 $06 $00 $0C $00 $00 $00 $00 $00 $00 $00
 
-TITLE_FRAME3
+GFX_TITLE_FRAME3
 	.by $00 $00 $00 $00 $00 $00 $00 $09 $06 $00 $09 $0C $00 $06 $00 $09 $0C $36 $00 $09 $C3 $69 $06 $9C $30 $00 $00 $00 $00 $00 $00 $00
 	.by $00 $00 $00 $00 $00 $00 $00 $C0 $03 $90 $06 $03 $00 $03 $00 $9C $06 $00 $90 $0C $00 $00 $03 $00 $06 $00 $00 $00 $00 $00 $00 $00
 	.by $00 $00 $00 $00 $00 $00 $00 $03 $0C $0C $03 $06 $00 $C0 $06 $03 $03 $00 $0C $03 $06 $90 $0C $00 $09 $00 $00 $00 $00 $00 $00 $00
@@ -288,7 +298,7 @@ TITLE_FRAME3
 	.by $00 $00 $00 $00 $00 $00 $00 $09 $06 $00 $09 $0C $60 $0C $06 $39 $09 $00 $06 $09 $00 $00 $06 $00 $06 $00 $00 $00 $00 $00 $00 $00
 	.by $00 $00 $00 $00 $00 $00 $00 $0C $03 $00 $06 $03 $00 $09 $00 $0C $06 $03 $C9 $0C $36 $9C $03 $00 $09 $00 $00 $00 $00 $00 $00 $00
 
-TITLE_FRAME4
+GFX_TITLE_FRAME4
 	.by $00 $00 $00 $00 $00 $00 $00 $06 $03 $00 $06 $09 $00 $03 $00 $06 $09 $C3 $00 $06 $9C $36 $03 $69 $C0 $00 $00 $00 $00 $00 $00 $00
 	.by $00 $00 $00 $00 $00 $00 $00 $90 $0C $60 $03 $0C $00 $0C $00 $69 $03 $00 $60 $09 $00 $00 $0C $00 $03 $00 $00 $00 $00 $00 $00 $00
 	.by $00 $00 $00 $00 $00 $00 $00 $0C $09 $09 $0C $03 $00 $90 $03 $0C $0C $00 $09 $0C $03 $60 $09 $00 $06 $00 $00 $00 $00 $00 $00 $00
@@ -300,6 +310,8 @@ TITLE_FRAME4
 ; Scrolling text for the directions, credits, etc.
 ; Since this is in Mode 6 it only needs 
 ; 20 blank characters for padding.
+
+GFX_SCROLL_CREDIT
 scrtxt   
 	.sb "                    PRESS FIRE TO PLAY"
 	.sb "     FIRE SHOOTS AND CHANGES CANNON DIRECTION"
@@ -311,25 +323,74 @@ scrtxt
 	.sb "                    "
 
 
+; Since Mode 6 characters are twice the width, then only half the 
+; screen data is needed.  What to do with the rest....?
+; I'd prefer to not remove the data.
+;
+; I thought about having the background scroll left and right 
+; in the opposite direction of the gun a simple parallax.
+; But this can only work for single player games.
+;
+; Last option is just to reposition the background randomly 
+; for the start of each game.   Having the credits show the 
+; animated title first, and then fade in the rest of the screen 
+; provides that transition time needed to randominze the 
+; ground position.
+;
+; The ground position can be LMS offset +0 to +36.
+; and horizontal fine scroll any value 0 to 15.
+; 
+; The bumper line can be a fixed 20 character line while 
+; the actual bumpers will be Missiles (Player 5).
+
+; Actual character values of mountains and ground lines...
+
 ; " ]              ]           ]         ] "
-; "[^\[        [  [^\       [\[^\]     ][^\"
+; "[^\[        ]  [^\       [\[^\]     ][^\"
 ; "]  \\    [\[^\[   \     [  \ [^\   [^\ ]"
 ; "_\   \  [  \ [     \   [        \ [   [_"
 ; "*______________________________________*" <- Bumpers
 ; "^^^^^^^^^^^^^^^          ^^^^^^^^^^^^^^^"
+;
+; Ascii art version of mountains to make it 
+; a little bit more legible...
+;
+; " ^              ^           ^         ^ "
+; "/T\^        ^  /T\       /\/T\^     ^/T\"
+; "^  \\    /\/T\/   \     /  \ /T\   /T\ ^"
+; "_\   \  /  \ /     \   /        \ /   /_"
 
-mountc   ; mountain screen view chars
-	.byte $20,$5d,$20,$20,$20,$20,$20,$20,$20,$20,$20,$20,$20,$20,$20,$20,$5d,$20,$20,$20,$20,$20,$20,$20,$20,$20,$20,$20,$5d,$20,$20,$20,$20,$20,$20,$20,$20,$20,$5d,$20
+mountc	; mountain screen view chars
+		; Note all values modified -$20 for Atari character codes.
+GFX_MOUNTAINS1
+	.byte $00,$3d,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$3d,$00,$00,$00
+	.byte $00,$00,$00,$00,$00,$00,$00,$00,$3d,$00,$00,$00,$00,$00,$00,$00,$00,$00,$3d,$00
 
-	.byte $5b,$5e,$5c,$5d,$20,$20,$20,$20,$20,$20,$20,$20,$5d,$20,$20,$5b,$5e,$5c,$20,$20,$20,$20,$20,$20,$20,$5b,$5c,$5b,$5e,$5c,$5d,$20,$20,$20,$20,$20,$5d,$5b,$5e,$5c
+GFX_MOUNTAIN2
+	.byte $3b,$3e,$3c,$3d,$00,$00,$00,$00,$00,$00,$00,$00,$3d,$00,$00,$3b,$3e,$3c,$00,$00
+	.byte $00,$00,$00,$00,$00,$3b,$3c,$3b,$3e,$3c,$3d,$00,$00,$00,$00,$00,$3d,$3b,$3e,$3c
 
-	.byte $5d,$20,$20,$5c,$5c,$20,$20,$20,$20,$5b,$5c,$5b,$5e,$5c,$5b,$20,$20,$20,$5c,$20,$20,$20,$20,$20,$5b,$20,$20,$5c,$20,$5b,$5e,$5c,$20,$20,$20,$5b,$5e,$5c,$20,$5d
+GFX_MOUNTAIN3
+	.byte $3d,$00,$00,$3c,$3c,$00,$00,$00,$00,$3b,$3c,$3b,$3e,$3c,$3b,$00,$00,$00,$3c,$00
+	.byte $00,$00,$00,$00,$3b,$00,$00,$3c,$00,$3b,$3e,$3c,$00,$00,$00,$3b,$3e,$3c,$00,$3d
 
-	.byte $5f,$5c,$20,$20,$20,$5c,$20,$20,$5b,$20,$20,$5c,$20,$5b,$20,$20,$20,$20,$20,$5c,$20,$20,$20,$5b,$20,$20,$20,$20,$20,$20,$20,$20,$5c,$20,$5b,$20,$20,$20,$5b,$5f
+GFX_MOUNTAINS4
+	.byte $3f,$3c,$00,$00,$00,$3c,$00,$00,$3b,$00,$00,$3c,$00,$3b,$00,$00,$00,$00,$00,$3c
+	.byte $00,$00,$00,$3b,$00,$00,$00,$00,$00,$00,$00,$00,$3c,$00,$3b,$00,$00,$00,$3b,$3f
 
-	.byte $40,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$5f,$40
+GFX_BUMPERLINE
+	.byte $3f,$3f,$3f,$3f,$3f,$3f,$3f,$3f,$3f,$3f,$3f,$3f,$3f,$3f,$3f,$3f,$3f,$3f,$3f,$3f
 
-	.byte $5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e,$5e
+; 24 |]]]]]]]]]]]]]]]00 0000 00[[[[[[[[[[[[[[[| Ground, stats - line, score value, hits left
+
+GFX_STATSLINE  ; "          L:00   PT:0000   H:00         "  ; "L:", "PT:", "H:" Use P/M graphics
+	.sb "            "
+GFX_STAT_LINE
+	.sb "00      "
+GFX_STAT_POINTS
+	.sb "0000     "
+GFX_STAT_HITS
+	.sb "00         " 
 
 
 
