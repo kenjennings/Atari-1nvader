a70b0a17b69b3cb38b4e7cec84919f0ce0273a46
Ken Jennings <kj.sumter.42@gmail.com>
2021-08-01 23:58:01 -0400

    Still working on NTSC v PAL colors.
    Realized that I don't need to keep secondary  lookup tables for both NTSC and PAL to copy to the master table of colors.  I only need the table for PAL, and then keep the master table default contents as NTSC.
    Realized a couple "colors" are really index into other tables, and put them back where they were originally declared in the code.

--- ata_1nv_page0.asm
+++ ata_1nv_page0.asm
@@ -122,7 +122,7 @@ zLandPhase       .byte $00  ; 0 == waiting  1 == scrolling.
 zLandMotion      .byte $00  ; 0 == left/right !0 == right/left
 
 zLandHS          .byte 0   ; fine horizontal scroll value start.
-;zLandColor       .byte 0   ; index for repeat DLIs on the scrolling land 
+zLandColor       .byte 0   ; index for repeat DLIs on the scrolling land 
 
 
 ; Flickering Stars values ==================================================
@@ -246,9 +246,9 @@ zPLAYER_DEBOUNCE
 zPLAYER_ONE_DEBOUNCE .byte $00 ; Set when laser is shot.   cleared when button released.  Cannot shoot again unless button released.
 zPLAYER_TWO_DEBOUNCE .byte $00 ; Set when laser is shot.   cleared when button released.  Cannot shoot again unless button released.
 
-;zPLAYER_COLOR
-;zPLAYER_ONE_COLOR  .byte $00 ; Player 1 current color 
-;zPLAYER_TWO_COLOR  .byte $00 ; Player 2 current color 
+zPLAYER_COLOR
+zPLAYER_ONE_COLOR  .byte $00 ; Player 1 current color loaded from table.
+zPLAYER_TWO_COLOR  .byte $00 ; Player 2 current color loaded from table.
 
 zPLAYER_BUMP
 zPLAYER_ONE_BUMP   .byte $00 ; Player 1 direction change is flagged
